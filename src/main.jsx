17d9b90d201cc484336c8f6dad68f7fa9e76b311
Ummadi Ajay <ummadiaj@gmail.com>
2026-02-14 19:04:42 +0530

    Fix blank screen: add basename to router and use relative paths

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { ToastProvider } from './context/ToastContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/RecordPoint">
       <ThemeProvider>
         <OnlineProvider>
           <ToastProvider>
